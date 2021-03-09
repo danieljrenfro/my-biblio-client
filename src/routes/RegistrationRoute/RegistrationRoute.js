33ed1c7d3326384b1eb00461e2ab9da883a51629
danieljrenfro <djrenfro95@gmail.com>
2021-03-09 06:21:08 -0600

    add registration page styles

--- src/routes/RegistrationRoute/RegistrationRoute.js
+++ src/routes/RegistrationRoute/RegistrationRoute.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
 
+import './RegistrationRoute.css';
+
 class RegistrationRoute extends Component {
   static defaultProps = {
     history: {
